1408297eef753b7e665dcdbc5c13ee359f017cd2
Steven Devine <stevenmichaeldevine@gmail.com>
2015-04-09 09:53:19 -0500

    php deploy cases

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,53 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :application, 'applicationname'
+set :scm, :git
+set :repo_url, 'git@bitbucket.org:username/project.git'
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
+
+# set :linked_dirs, x=fetch(:linked_dirs)?x:[] + %w{what/ever public/uploads}
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Make Artisan executable"
+  task :artisan_executable do
+    on roles(:app) do
+      within release_path do
+        execute :chmod, "u+x artisan"
+      end
     end
   end
 
+  desc "Run Laravel migrations (if any)"
+  task :migrations do
+    on roles(:app) do
+      within release_path do
+        execute :php, "artisan migrate"
+      end
+    end
+  end
+
+  desc "Sets set of settable settings"
+  task :config_mv do
+    on roles(:app) do
+      within release_path do
+        execute :mv, "#{fetch(:env_file)} .env"
+      end
+    end
+  end
+
+  # move config files for poorly designed systems
+  after :deploy, "deploy:config_mv"
+
+  after :deploy, "deploy:artisan_executable"
+  after :deploy, "deploy:migrations"
+
+  # check/create symlinked folders
+  after :deploy, "deploy:check:linked_dirs"
+  after :deploy, "deploy:symlink:shared"
+
 end
+
